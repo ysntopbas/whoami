050bf29c9ede394aae5aca53938a4aedb50340f0
ysntopbas <ysntpbss@gmail.com>
2025-02-10 16:01:16 +0300

    Add app icon using flutter_launcher_icons package

--- lib/features/game/presentation/pages/game_results_page.dart
+++ lib/features/game/presentation/pages/game_results_page.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:whoami/core/utils/orientation_manager.dart';
 import 'package:whoami/features/category/domain/models/category_model.dart';
